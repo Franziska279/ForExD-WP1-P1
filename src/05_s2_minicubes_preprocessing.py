9e2d3edf427192bd8ae22ba1f1c1a161fdca0ca5
Franziska Müller <fmueller@bgc-jena.mpg.de>
2024-05-24 15:40:42 +0200

    add s2 preprocessing files

---
+++ src/05_s2_minicubes_preprocessing.py
@@ -0,0 +1,217 @@
+import xarray as xr
+import numpy as np
+import os
+import sys
+import pystac_client
+import planetary_computer as pc
+import stackstac
+import earthnet_minicuber as emc
+import rasterio
+import logging  
+import geopandas as gpd
+import mypythonlib
+from mypythonlib import myfunctions, phenolopy
+
+# Hide warnings (many since some xarray class uses some deprecated python function on 3.9)
+import warnings
+warnings.filterwarnings('ignore')
+
+def generate_output_folder(idx, dir):
+
+    # Define the folder to save the NetCDF files
+    output_folder = f"/{dir}/{idx}"
+    # Ensure the output folder exists
+    os.makedirs(output_folder, exist_ok=True)
+
+    return output_folder
+
+
+
+def minicuber_download(idx, intersecting_grid_gdf_events_unique, output_folder):
+
+    first_polygon = intersecting_grid_gdf_events_unique.geometry.iloc[idx]
+
+
+    lon = first_polygon.centroid.x
+    lat = first_polygon.centroid.y
+    start_year = 2015
+    last_year = 2023
+
+    import os
+
+    # Iterate over all years from prev_year to next_year
+    for year in range(start_year, last_year + 1):
+        try:
+            # Check if the NetCDF file for the current year already exists
+            filename = os.path.join(output_folder, f"{idx}_{year}.nc")
+            if os.path.exists(filename):
+                print(f"Minicube for year {year} already exists. Skipping...")
+                continue
+
+            print(f"Loading Minicube for year {year}")
+            specs = {
+                "lon_lat": (lon, lat), # center pixel
+                "xy_shape": (1292, 1292), # width, height of cutout around center pixel
+                "resolution": 20, # in meters.. will use this on a local UTM grid..
+                "time_interval": f"{year}-01-01/{year}-12-31",
+                "providers": [
+                    {
+                        "name": "s2",
+                        "kwargs": {"bands": ["B01","B02", "B03", "B04", "B05", "B06", "B07", "B08", "B8A", "B09", "B10", "B11", "B12"], "best_orbit_filter": True, "five_daily_filter": True, "brdf_correction": True, "cloud_mask": True, "cloud_mask_rescale_factor": 2, "aws_bucket": "planetary_computer"}
+                    }
+                    ]
+            }
+
+            mc = emc.load_minicube(specs, compute=True)
+            
+            # Save minicube to a NetCDF file
+            filename = os.path.join(output_folder, f"{idx}_{year}.nc")
+            print(f"Saving minicube for year {year} to {filename}")
+            comp = dict(zlib=True, complevel=9)
+            encoding = {var: comp for var in mc.data_vars}
+            print(encoding)
+
+            mc.to_netcdf(filename, encoding=encoding)
+        
+        except Exception as e:
+            logging.debug(f"The file {idx} for year {year} did not have an id. Error: {e}")
+
+    print("End download")
+
+
+
+def merge_minicubes(idx, output_folder):
+    # Expected years range
+    expected_years = range(2015, 2024)
+
+    # List of expected file names
+    expected_files = [f"{idx}_{year}.nc" for year in expected_years]
+
+    # Get all NetCDF files in the output folder
+    nc_files = [file for file in os.listdir(output_folder) if file.endswith('.nc')]
+
+    # Check if all expected files are present
+    missing_files = [file for file in expected_files if file not in nc_files]
+    if missing_files:
+        print(f"Missing files: {missing_files}")
+        print(f"Breaking of the merging .... ")
+        
+    else:
+        # All files are present, proceed with merging
+        merged_filename = os.path.join(output_folder, f"{idx}_merged.nc")
+
+        print(f"Merging all NetCDF files into {merged_filename}")
+
+        # Open and merge all NetCDF files along the time axis
+        merged_ds = xr.open_mfdataset([os.path.join(output_folder, file) for file in nc_files], combine='nested', concat_dim='time')
+        merged_ds.to_netcdf(merged_filename)
+        print("End merging")
+        print("\n Start preprocessing:")
+        preprocess_and_reduce_minicube(merged_ds, idx, output_folder)
+
+def preprocess_and_reduce_minicube(ds, idx, output_folder):
+
+    ds = ds.rename_dims({'lon': 'x', 'lat': 'y'})
+    ds.rio.write_crs("epsg:4326", inplace=True)  # Set the coordinate reference system to EPSG:4326
+
+    print(f"Remove clouds, shadows, and snow")
+    # Calculate the fraction of 1 values in s2_mask for each time step
+    mask_fraction = ds.s2_mask.where(ds.s2_mask == 0, 1).sum(dim=['x', 'y']) / (len(ds.x) * len(ds.y))
+
+    # Filter the data to include only bands with names starting with 's2_B'
+    filtered_data = ds[[b for b in ds.variables if 's2_B' in b]]
+
+            # Mask out pixels where s2_mask is not equal to 0
+    filtered_data = filtered_data.where(ds.s2_mask == 0)
+
+    # Remove cirrus clouds from the entire dataset
+    filtered_data = phenolopy.remove_cirrus_clouds(filtered_data)
+
+    print(f"Remove outliers")
+    # Remove outliers using a specified method
+    remove_outliers = phenolopy.remove_outliers(filtered_data, method='median', user_factor=2, z_pval=0.07)
+
+    print(f"Compute various vegetation indices")
+    # Compute various vegetation indices and add them to the dataset
+    print('NDVI')
+    remove_outliers['ndvi'] = myfunctions.ndvi(remove_outliers)
+    print('NBR')
+    remove_outliers['nbr'] = myfunctions.nbr(remove_outliers)
+    print('NDWI')
+    remove_outliers['ndwi'] = myfunctions.ndwi(remove_outliers)
+    print('NDRE')
+    remove_outliers['ndre'] = myfunctions.ndre(remove_outliers)
+    print('TCW')
+    remove_outliers['tcw'] = myfunctions.tcw(remove_outliers)
+    print('TCG')
+    remove_outliers['tcg'] = myfunctions.tcg(remove_outliers)
+    print('TCB')
+    remove_outliers['tcb'] = myfunctions.tcb(remove_outliers)
+    print('NDMI')
+    remove_outliers['ndmi'] = myfunctions.ndmi(remove_outliers)
+    print('NIRV')
+    remove_outliers['nirv'] = myfunctions.nirv(remove_outliers)
+    print('kNDVI')
+    remove_outliers['kndvi'] = myfunctions.kndvi(remove_outliers, sigma=0.02)
+    print('DRS/NDRS')
+    remove_outliers['drs'] = myfunctions.drs(remove_outliers)
+    remove_outliers['ndrs'] = myfunctions.ndrs(remove_outliers)
+    print('kDRS/kNDRS')
+    remove_outliers['kdrs'] = myfunctions.kdrs(remove_outliers, sigma=0.02)
+    remove_outliers['kndrs'] = myfunctions.kndrs(remove_outliers)
+
+    print(f"Sort temporal axis")
+    remove_outliers = remove_outliers.sortby('time')
+
+    print(f"Interpolate linearly over time")
+    # Interpolate missing values linearly over time
+    interpolated_data = remove_outliers.interpolate_na(dim='time', method='linear')
+
+    print(f"Smooth data with Savitzky-Golay (window_length=15, polyorder=3)")
+    # Smooth the data using the Savitzky-Golay filter
+    smoothed_data = phenolopy.smooth(ds=interpolated_data, method='savitsky', window_length=15, polyorder=3)
+
+    print(f"Calculate Anomaly for each variable")
+    for var in smoothed_data.data_vars:
+            print(var)
+            smoothed_data[var+'_anom']     = smoothed_data[var].groupby('time.week')-smoothed_data[var].groupby('time.week').median()
+
+    # Save the preprocessed data to a NetCDF file
+    print(f"Compress data for saving to complevel=9")
+    comp = dict(zlib=True, complevel=9)
+    encoding = {var: comp for var in smoothed_data.data_vars}
+    print(encoding)
+    outputpath = os.path.join(output_folder, f"{idx}_merged_compressed.nc")
+    smoothed_data.to_netcdf(outputpath, encoding=encoding)
+
+    print(f"Saved to path = {outputpath}")
+
+    
+def main():
+    # Check if correct number of arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: python minicube_download_preprocessing_pipeline.py SLURM_ARRAY_TASK_ID INPUT_FILE OUTPUT_DIR")
+        sys.exit(1)
+
+    # Parse command line arguments
+    slurm_array_task_id = sys.argv[1]
+    input_file = sys.argv[2]
+    output_dir = sys.argv[3]
+
+    print(f"Running Task ID {slurm_array_task_id}")
+
+    index = int(slurm_array_task_id)
+    gdf_grid = gpd.read_file(input_file)
+
+    output_folder = generate_output_folder(index, output_dir)
+    # Run the function
+    minicuber_download(index, gdf_grid, output_folder)
+    merge_minicubes(index, output_folder)
+
+    print('\nDone')
+
+if __name__ == "__main__":
+
+    main()
+
+
