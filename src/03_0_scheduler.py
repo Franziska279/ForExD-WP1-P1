c66586039a19b0f58fb19b8d58f476b09d6a1985
Franziska Müller <fmueller@bgc-jena.mpg.de>
2024-06-28 10:31:50 +0200

    adding all the paths as global vars

--- src/03_0_scheduler.py
+++ src/03_0_scheduler.py
@@ -1,4 +1,25 @@
-                
+"""
+S1CD Preprocessing and Relabeling Scheduler Script
+=======================================
+
+Author: Franziska Müller
+Date: 28.06.2024
+
+This script schedules and executes the preprocessing and relabeling of USDA disturbance data for Region 8 using radar data. 
+The main tasks of the script are:
+
+1. Setting Up Logging: Configures logging to record the progress and any errors encountered during execution.
+2. Defining Constants: Specifies important constants such as file paths and maximum jobs for parallel processing.
+3. Running Extraction Script: Executes a Python script to process individual files, 
+    converting and relabeling data based on radar change detection.
+4. Concurrent Processing: Utilizes concurrent processing to handle multiple files in parallel, improving efficiency.
+5. Tracking Progress: Uses a progress bar to track and display the number of successfully processed files and errors.
+6. Merging Results: After processing all files, calls a merge script to combine the results into a final output.
+
+Results are saved in the specified directory with a confirmation message printed upon completion. 
+The script ensures robust error handling and logs detailed information for troubleshooting.
+"""
+
 import os
 import sys
 import subprocess
@@ -11,6 +32,10 @@ MAX_JOBS = 20
 INPUT_PATHS = "/Net/Groups/BGI/work_2/ForExD/WP1/Data/s1_change_detection_northamerica/"
 IDS_USDA_PATH = "/Net/Groups/BGI/scratch/fmueller/ForExD-WP1-P1/results/region8_dca_filtered_ids_usda_polygons.csv"
 OUTPUT_FILENAME = "radar_enhanced_forest_disturbance_mapping.shp"
+SHAPEFILES_DIR = "/Net/Groups/BGI/scratch/fmueller/ForExD-WP1-P1/results/03_s1cd_polygons/"
+OUTPUT_DIR = "/Net/Groups/BGI/scratch/fmueller/ForExD-WP1-P1/results/"
+TARGET_CRS = "EPSG:4326"
+
 
 # Logging setup
 logging.basicConfig(filename='scheduler.log', level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -67,7 +92,7 @@ def main():
     # After all jobs are finished, call the merge script
     print("All jobs completed. Calling merge script...")
     try:
-        subprocess.run(['python', '03_2_merge_s1cd_files.py'], check=True)
+        subprocess.run(['python', '03_2_merge_s1cd_files.py', SHAPEFILES_DIR, OUTPUT_DIR, OUTPUT_FILENAME, TARGET_CRS], check=True)
         print("Merge script executed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error executing merge script: {e}")
